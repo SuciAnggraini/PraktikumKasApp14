5fb872c2176e4206ea05e95ad07bc2305d02609c
SuciAnggraini <nataliaangraini013@gmail.com>
2020-12-20 13:29:43 +0800

    '
    "first"

---
+++ app/src/main/java/com/Suci/kasapp/helper/CurrentDate.java
@@ -0,0 +1,10 @@
+package com.Suci.kasapp.helper;
+
+import java.util.Calendar;
+
+public class CurrentDate {
+    public static Calendar calander = Calendar.getInstance();
+    public static int year          = calander.get(Calendar.YEAR );
+    public static int mont          = calander.get(Calendar.MONTH );
+    public static int day          = calander.get(Calendar.DAY_OF_MONTH );
+}
